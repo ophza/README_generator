09a93f718a77e5e955a8da6eacab281dc61ebfb1
ophza <jonathanlet@icloud.com>
2020-10-15 21:28:48 -0600

    Completed Code

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 const util = require("util");
 
 // Questions for the user
-const questions = [
+const form = [
    {
       type: "input",
       message: "Enter project title:",
@@ -22,17 +22,17 @@ const questions = [
    {
       type: "input",
       message: "Explain the usage:",
-      name: "installation"
+      name: "usage"
    },
    {
       type: "input",
-      message: "List collaborators (leave blank if none):",
+      message: "List collaborators (seperate with pipe | leave blank if none):",
       name: "collaborators"
    },
    {
       type: "input",
-      message: "Enter liscense type:",
-      name: "liscense"
+      message: "Enter license type:",
+      name: "license"
    },
    {
       type: "input",
@@ -47,6 +47,58 @@ const questions = [
    {
       type: "input",
       message: "Enter some frequently asked questions and their answers:",
-      name: "test"
+      name: "questions"
    }
 ];
+
+function generateREADME(data){
+return `
+# ${data.title}
+
+## Table Of Contents
+- [Description](##Description:)
+- [Installation](##Installation)
+- [Usage](##Usage)
+- [Collaborators](##Collaborators)
+- [Licensing](##Licensing)
+- [Contribute](##How-to-Contribute)
+- [Test](##How-to-Test)
+- [Questions](##Questions)
+
+## Description:
+${data.description}
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+
+## Collaborators
+${data.collaborators}
+
+## Licensing
+${data.license}
+
+## How to Contribute
+${data.contributeGuide}
+
+## How to Test
+${data.test}
+
+## Questions
+${data.questions}
+
+###### GENERATED USING JON LETOURNEAU'S README GENERATOR
+`
+}
+
+inquirer.prompt(form).then(response => {
+   const finalREADME = generateREADME(response)
+
+   fs.writeFile("README(generated).md", finalREADME, function(err){
+      if(err) return console.log("ERROR GENERATING README")
+
+      console.log("SUCCESS!")
+   })
+})
