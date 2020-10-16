52f36ef2b996cca407db2bd09faa9f2804faeaff
ophza <jonathanlet@icloud.com>
2020-10-16 15:14:55 -0600

    cleaned up some code

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 const util = require("util");
+const { makeBadge, ValidationError } = require('badge-maker')
 
 // Questions for the user
 const form = [
